40a42683165f10d1802355086934f248091fe7e8
Iuliia23 <117689978+Iuliia23@users.noreply.github.com>
2023-09-09 01:19:05 +0200

    commit_2

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { useFonts } from 'expo-font';
+import LoginScreen from './Screens/authScreens/LoginScreen/LoginScreen';
+import RegistrationScreen from './Screens/authScreens/RegistrationScreen/RegistrationScreen';
+
 
 export default function App() {
+  const [page, setPage ] = useState("registration");
+
+  const changePage = data => {
+    setPage(data);
+  };
+
+  const [fontsLoaded] = useFonts({
+    "Roboto-Black": require("./assets/fonts/Roboto/Roboto-Black.ttf"),
+    "Roboto-Medium": require("./assets/fonts/Roboto/Roboto-Medium.ttf"),
+    "Roboto-Regular": require("./assets/fonts/Roboto/Roboto-Regular.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    (page == "registration" && (
+      <RegistrationScreen changePage={() => changePage("login")} />
+    )) ||
+    (page == "login" && (
+      <LoginScreen setPage changePage={() => changePage("registration")} />
+    ))
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
